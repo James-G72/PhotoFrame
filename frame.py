ca94dbf6d5e9f22eb980299acef3f54352049449
James Gower <james.gower@cmrsurgical.com>
2022-08-30 08:37:57 +0100

    Overhauling the format to add an unpack function for handling the assembley of the imgs variable. Also improved the way in which the shuffle works to shuffle a possitions list instead of the actual image list which now remains untouched

--- frame.py
+++ frame.py
@@ -9,92 +9,55 @@ import datetime
 
 class PhantomFrame(tk.Frame):
     """
-    The main host that is called to start the game
+    PhantomFrame is the host class for the logic that handles the pictures
     """
 
-    def __init__(self, parent, target_folder="",timer=5,shuffle=True):
+    def __init__(self, parent, target_folder="", timer=5, shuffle=True):
         '''
         The PhantomFrame object the slideshow code.
-        :param parent: The tk root window inside of which you want the board to be drawn
-        :param target_folder: The folder to pull photos from
-        :param timer: The duration between photos
-        :param shuffle: A bool as to whether or not to play photos in order
+        :param parent: The tk root window inside of which you want the pictures to be drawn
+        :param target_folder: The folder to pull photos from as the "Main" folder. This can contain folders within it
+        :param timer: The duration between photos in seconds
+        :param shuffle: A bool as to whether or not to play photos in order, or shuffle them. (Long term I would like to
+                        move this decision to which port the USB stick is plugged into.
         '''
-        # There is no need to edit any of the sizes. The default for side_size is 200
-        # The default colors here are pure white and a dark gray
+        # ---------------- Section 1 : Unpacking the images ----------------
+        # All of the images are processed upon start-up and placed into a list
+        # Additional information is also extracted and stored such that they can later be overlayed
 
-        # ---------------- Section 1 : Assembling basic variables ----------------
-        # Adding all of the pictures from Images folder
-        # self.imageHolder = {}  # Creating a dictionary
-        # pieceList = "0123456789"  # These are all the different types of pieces possible
-        # for f in pieceList:  # Cycling through the pieces
-        #     for add in ["","_mini"]:
-        #         with open("Images/"+f+add+".png","rb") as imageFile:  # Opening the photo within the variable space
-        #             # The images can't be stored as P or p in MacOS as they're read the same
-        #             # So the colour is introduced by adding b or w after the piece notation
-        #             string = base64.b64encode(imageFile.read())  # Creating a string that describes the gif in base64 format
-        #             self.imageHolder[f+add] = tk.PhotoImage(data=string)
-
-        # ---------------- Section 2 : Creating the board ----------------
-        # The whole board is drawn within the window in TkInter
-        # This a very long section defining a lot of stationary visuals for the GUI
-        # Most of the placement is just done by eye to make sure it all looks okay
-        imgs = []
-        origin = []
-        date = []
-        valid_images = [".jpg",".gif",".png",".tga"]
-        # target_folder = "D:\\"
+        # Defining all of the lists that hold images and information.
+        self.imgs = []
+        self.origin = []
+        self.date = []
+        self.folder_stats = []
+        # These are the types of image that can currently be handled
+        self.valid_images = [".jpg",".gif",".png",".tga"]
+        target_folder = "D:\\" # This is required to run the code on my laptop
+        # This pulls all immediate folders from the location defined as "Main"
         folders = next(os.walk(target_folder))[1]
         if "System Volume Information" in folders:
             folders.remove("System Volume Information")
+        # If no folders are found then the photos can all be unpacked from a single folder
         if len(folders) == 0:
             unpack_mode = "single_folder"
         else:
             unpack_mode = "multiple_folders"
-        # Unpacking images into a list
 
+        # Unpacking images into a list
         if unpack_mode == "single_folder":
-            for f in os.listdir(target_folder):
-                if f[0] != ".":
-                    ext = os.path.splitext(f)[1]
-                    if ext.lower() not in valid_images:
-                        continue
-                    imgs.append(Image.open(os.path.join(target_folder,f)))
-                    origin.append("Main")
-                    try:
-                        date.append(Image.open(target_folder)._getexif()[36867])
-                    except:
-                        date.append("Unknown")
+            # Calling a function that appends all images found in that folder
+            self._unpack(target_folder, "Main")
         else:
-            for f in os.listdir(target_folder):
-                if f[0] != ".":
-                    ext = os.path.splitext(f)[1]
-                    if ext.lower() not in valid_images:
-                        continue
-                    imgs.append(Image.open(os.path.join(target_folder,f)))
-                    origin.append("Main")
-                    try:
-                        date.append(Image.open(os.path.join(target_folder,f))._getexif()[36867])
-                    except:
-                        date.append("Unknown")
-            for x in range(0,len(folders)):
-                for f in os.listdir(target_folder+folders[x]):
-                    if f[0] != ".":
-                        ext = os.path.splitext(f)[1]
-                        if ext.lower() not in valid_images:
-                            continue
-                        imgs.append(Image.open(os.path.join(target_folder+folders[x], f)))
-                        origin.append(folders[x])
-                        try:
-                            date.append(Image.open(os.path.join(target_folder,f)+folders[x])._getexif()[36867])
-                        except:
-                            date.append("Unknown")
-        self.list = imgs
-        self.directories = origin
-        self.dates = date
-        self.image_num = len(self.list)
+            self._unpack(target_folder, "Main")
+            # Cycling through all folders that were found and extracting from them
+            for x in range(0, len(folders)):
+                self._unpack(target_folder+folders[x], folders[x])
+
+        self.image_num = len(self.imgs)
+        # Defining a list of positions to shuffle such that we don't have to shuffle the actual lists
+        self.pos_list = [x for x in range(0, self.image_num)]
         if shuffle:
-            random.shuffle(self.list)
+            random.shuffle(self.pos_list)
         self.timer = timer
         self.pos = 0
 
@@ -103,23 +66,67 @@ class PhantomFrame(tk.Frame):
         self.date_font = ImageFont.truetype("theboldfont.ttf", 25)
 
         # Creating the canvas for the window
-        tk.Frame.__init__(self,parent)
+        tk.Frame.__init__(self, parent)
         # This is self explanatory and provides a blank space upon which visual objects can be placed
         self.c_width = self.winfo_screenwidth()
         self.c_height = self.winfo_screenheight()
         self.canvas = tk.Canvas(self,borderwidth=0,highlightthickness=0,width=self.c_width,height=self.c_height,background="black")
-        self.canvas.pack(side="top",fill="both",expand=True)  # Packed with a small amount of padding either side
+        self.canvas.pack(side="top", fill="both", expand=True)  # Packed with a small amount of padding either side
+
+    def _unpack(self, target, folder_name):
+        """
+        Searches through directory and appends all suitable images found to self.imgs
+        :param target: Location in which to search for images
+        :param folder_name: The name to be appended to self.origin
+        :return: None
+        """
+        # Defining variables for tracking the first and last date in the folder
+        first_date = datetime.datetime.now()
+        # Setting a time a very long time in the past (1600s)
+        last_date = datetime.datetime.now() - datetime.timedelta(days=150000)
+        # For all files in the directory
+        for f in os.listdir(target):
+            if f[0] != ".":
+                ext = os.path.splitext(f)[1]
+                # Checking the filetype is supported
+                if ext.lower() not in self.valid_images:
+                    continue
+                # Appending the image file to the list
+                self.imgs.append(Image.open(os.path.join(target, f)))
+                # Appending the folder name
+                self.origin.append(folder_name)
+                try:
+                    # Attempt to pull the date exif data
+                    exif = Image.open(os.path.join(target,f))._getexif()[36867]
+                    # Turn the data into a datetime object
+                    date_time_object = datetime.datetime.strptime(exif.split(" ")[0], '%Y:%m:%d')
+                    # Format it in a better way
+                    proper_date = date_time_object.strftime("%d-%B %Y")
+                    self.date.append(proper_date)
+                    # Check if this date will change the extremes in anyway
+                    if date_time_object < first_date:
+                        first_date = date_time_object
+                    if date_time_object > last_date:
+                        last_date = date_time_object
+                except:
+                    # If the above fails then just append a blank date
+                    self.date.append("")
+        # Set the folder stats
+        self.folder_stats.append([first_date.strftime("%d-%B %Y"), last_date.strftime("%d-%B %Y")])
 
     def _run_image(self):
-        self.image = self.list[self.pos]
-        self.date = self.dates[self.pos]
-        self.folder = self.directories[self.pos]
-        im = self._resized_image()
-        draw = ImageDraw.Draw(im)
-        proper_date = datetime.datetime.strptime(self.date.split(" ")[0], '%Y:%m:%d').strftime("%d-%B %Y")
-        draw.text((0, 0), self.folder, (255, 255, 255), font=self.folder_font)
-        draw.text((0, 45), proper_date, (255, 255, 255), font=self.date_font)
-        img = ImageTk.PhotoImage(im)
+        """
+        This function is self_calling and handles all of the visuals. Each image is cycled through
+        :return: None
+        """
+        # Pulling out the image that we want to show next
+        self.image = self.imgs[self.pos_list[self.pos]]
+        self.date_1 = self.date[self.pos_list[self.pos]]
+        self.folder = self.origin[self.pos_list[self.pos]]
+        self.prev_folder = "Main"
+        self.im = self._resized_image()
+        self.im_overlay = self._overlay()
+        img = ImageTk.PhotoImage(self.im_overlay)
         self.canvas.delete("image")
         self.canvas.create_image(self.c_width/2,self.c_height/2, image=img, anchor="c", tag="image") # First we create the image in the top left
         self.update()
@@ -128,13 +135,14 @@ class PhantomFrame(tk.Frame):
         else:
             self.pos += 1
         time.sleep(self.timer)
-        self.after(5000,self._run_image())
+        self.prev_folder = self.folder
+        self.after(5000, self._run_image())
 
     def _resized_image(self):
         self.img_copy = self.image.copy()
         image_width, image_height = self.image.size
-        HEIGHT,WIDTH = self.winfo_screenheight(), self.winfo_screenwidth()
-        print(image_width,image_height,HEIGHT,WIDTH)
+        HEIGHT, WIDTH = self.winfo_screenheight(), self.winfo_screenwidth()
+        print(image_width, image_height, HEIGHT, WIDTH)
         if image_width > WIDTH:
             w_f = image_width / WIDTH
             if image_height > HEIGHT:
@@ -162,4 +170,24 @@ class PhantomFrame(tk.Frame):
                 r_h = HEIGHT
                 r_w = math.floor(image_width / h_f)
 
-        return self.img_copy.resize((r_w,r_h))
+        return self.img_copy.resize((int(r_w),int(r_h)))
+
+    def _overlay(self):
+        """
+        Overlays text over the top of the image to show the folder name and capture date
+        :return: Image with the overlay on top
+        """
+        im_check = self.im.crop((0, 0, 100, 50))
+        npixels = im_check.size[0] * im_check.size[1]
+        cols = im_check.getcolors(npixels)
+        sumRGB = [(x[0] * x[1][0], x[0] * x[1][1], x[0] * x[1][2]) for x in cols]
+        avg = tuple([sum(x) / npixels for x in zip(*sumRGB)])
+        if sum(avg) < 300:
+            font_color = (255, 255, 255)
+        else:
+            font_color = (0, 0, 0)
+        draw = ImageDraw.Draw(self.im)
+        draw.text((1, 1), self.folder, font_color, font=self.folder_font)
+        draw.text((1, 46), self.date_1, font_color, font=self.date_font)
+
+        return self.im
